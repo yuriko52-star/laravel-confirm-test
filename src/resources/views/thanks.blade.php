04108a01f27dafde0e3b93db4844b946ad8023ab
yuriko <yuriko40223@gmail.com>
2024-10-29 14:10:46 +0900

    ファイル作成とapp thanks ファイルの入力

---
+++ src/resources/views/thanks.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FashionablyLate</title>
+    @section('css')
+    <link rel="stylesheet" href="{{ asset('css/thanks.css') }}">
+    @endsection
+
+    @section('content')
+    <div class="thanks__content">
+        <form action="" class="form" method="">
+            <div class="thanks__sentens">
+                <div class="お問い合わせありがとうございました"></div>
+                <div class="back__image">
+                    thank you
+                </div>
+            </div>
+            <div class="form__button">
+                <button class="form__button-submit" type="submit">HOME</button>
+            </div>
+        </form>
+    </div>
+
+</head>
+<body>
+    
+</body>
+</html>
